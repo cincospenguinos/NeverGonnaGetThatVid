79b6de15dd83213b57bdce205b47f438aa519401
Andre LaFleur <andre.lafleur@utah.edu>
2017-03-22 09:08:44 -0600

    Got the video GET request to work correctly

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # NeverGonnaGetThatVid
 
-This is an April Fool's joke I put together.
+This is an April Fool's joke I put together. Special thanks to my man Stefano
+for giving me the idea.
--- app.rb
+++ app.rb
@@ -1,39 +1,39 @@
 # app.rb
-#
-# The sinatra app for NeverGonnaGetThatVid
+
 require 'sinatra'
 require 'json'
 require 'yaml'
 require 'net/http'
 
+configure do
+  set :api_url, 'https://www.googleapis.com/youtube/v3/videos'
+  set :api_key, YAML.load_file('.api_key.yaml')['API_KEY'] # TODO: Don't do this
+  set :fields, 'items(snippet(title, thumbnails))'
+end
+
 helpers do
 
   def get_video_info(video_url)
     return nil if (video_id = extract_video_id(video_url)).nil?
-    url = URI.parse("#{settings.api_url_endpoint}?id=#{video_id}&key=#{settings.api_key}&part=snippet&fields=#{settings.fields}")
-    res = Net::HTTP.get(url)
-    JSON.parse(res)
+    url = URI.parse("#{settings.api_url}?id=#{video_id}&key=#{settings.api_key}&part=snippet&fields=#{settings.fields}")
+    res = JSON.parse(Net::HTTP.get(url))
+    res['url'] = video_url
+    res
   end
 
   def send_response(successful, message, video_info = nil)
-    # successful => bool, message => string, video_info => hash with details of the video
     { successful: successful, message: message, video_info: video_info }.to_json
   end
 
   def extract_video_id(video_url)
-    return nil unless video_url.match(/www.youtube.com\/watch\?v=[a-zA-Z0-9]+/) || video_url.match(/www.youtu.be\/[0-9A-Za-z]+/)
+    return nil unless video_url.match(/www.youtube.com\/watch\?v=\S+/) || video_url.match(/youtu.be\/\S+/)
+
     if video_url.match(/www.youtube.com\/watch\?v=[a-zA-Z0-9]+/)
-      video_url[/v=[a-zA-Z0-9]+/].gsub('v=', '')
+      video_url[/v=\S+/].gsub('v=', '')
     else
-      video_url[/\/[a-zA-Z0-9]+/].gsub('/', '')
+      video_url[/be\/\S+/].gsub('/', '').gsub('be', '')
     end
   end
-end
-
-configure do
-  set :api_url_endpoint, 'https://www.googleapis.com/youtube/v3/videos'
-  set :api_key, YAML.load_file('.api_key.yaml')['API_KEY']
-  set :fields, 'items(snippet(title, thumbnails))'
 end
 
 # The main page
@@ -46,16 +46,27 @@ get '/*' do
   # TODO: This
 end
 
+# Returns the status of the video provided
+get '/status/*' do
+
+end
+
 # The endpoint URL to "get" the video
 post '/' do
   if params.include?('video_url')
     id = get_video_info(params['video_url'])
 
     if id.nil?
-      send_response(false, 'Invalid URL! Could not get video ID!')
+      send_response(false, 'That is not a valid URL')
     else
-      # TODO: "Grab" the video
-      send_response(true, 'Grabbing the video...')
+      video_info = get_video_info(params['video_url'])
+
+      if video_info['items'].size == 0
+        send_response(false, 'Could not find a video at that URL')
+      else
+        # TODO: This is where we "grab" the video and setup a link to it
+        send_response(true, 'Grabbing the video...', video_info)
+      end
     end
   else
     send_response(false, 'No URL provided!')
