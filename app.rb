f13d1697f17fd4ec35c3e19adc06a3a55392d904
Andre LaFleur <andre.lafleur@utah.edu>
2017-03-22 07:46:28 -0600

    Add script and setup some things

--- app.rb
+++ app.rb
@@ -2,12 +2,36 @@
 #
 # The sinatra app for NeverGonnaGetThatVid
 require 'sinatra'
+require 'json'
+
+helpers do
+
+  def get_video_id(video_url)
+    nil
+  end
+
+  def send_response(successful, message)
+    { successful: successful, message: message }.to_json
+  end
+end
 
 # The main page
 get '/' do
-  'Hello, world!'
+  erb :index
 end
 
 # The endpoint URL to "get" the video
 post '/' do
+  if params.include?('video_url')
+    id = get_video_id(params['video_url'])
+
+    if id.nil?
+      send_response(false, 'Invalid URL! Could not get video ID!')
+    else
+      # TODO: "Grab" the video
+      send_response(true, 'Grabbing the video...')
+    end
+  else
+    send_response(false, 'No URL provided!')
+  end
 end
---
+++ public/script/app.js
@@ -0,0 +1,11 @@
+/**
+ * app.js
+ *
+ * All the javascript for the app
+ *
+ * TODO: All of this
+ */
+
+$(document).ready(function(){
+
+});
